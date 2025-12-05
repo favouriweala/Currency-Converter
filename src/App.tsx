a3fff1f34032c070b964e6b4addb15023a690f2a
favouriweala <favouriweala@gmail.com>
2025-12-05 14:36:54 +0100

    implement the simple structure of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-
+import Header from './components/Header'
+import CurrencyConverter from './components/CurrencyConverter'
 import './App.css'
 
 function App() {
 
   return (
     <>
-     
+      <Header/>
+      <CurrencyConverter/>
     </>
   )
 }
